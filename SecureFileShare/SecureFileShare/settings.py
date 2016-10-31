b4f01b85e71eba86231d43f5be9f625d7a71ab5b
Muthu <mc4xf@virginia.edu>
2016-10-31 13:42:28 -0400

    Added User profiles with Site Manager capabilities.

--- SecureFileShare/SecureFileShare/settings.py
+++ SecureFileShare/SecureFileShare/settings.py
@@ -127,3 +127,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- SecureFileShare/app/urls.py
+++ SecureFileShare/app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
+	url(r'^accounts/profile/edit', views.update_profile, name='update_profile'),
+	url(r'^accounts/profile', views.profile, name='profile'),
 	url(r'^$', views.index, name='index'),
 ]
