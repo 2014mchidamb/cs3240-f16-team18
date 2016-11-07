184c91c743728d328091f99a41326113ab0a2578
AgentE382 <AgentE382@gmail.com>
2016-11-07 10:53:45 -0500

    Fix model Profile reference

--- SecureFileShare/messenger/models.py
+++ SecureFileShare/messenger/models.py
@@ -5,6 +5,6 @@ from django.db import models
 # Create your models here.
 class Message(models.Model):
     text = models.TextField(max_length=500, blank=True)
-    sender = models.OneToOneField(User, on_delete=models.CASCADE)
-    recipient = models.OneToOneField(User, on_delete=models.CASCADE)
+    sender = models.OneToOneField(User, on_delete=models.Profile)
+    recipient = models.OneToOneField(User, on_delete=models.Profile)
     encryptedFlag = models.BooleanField(default=False)
