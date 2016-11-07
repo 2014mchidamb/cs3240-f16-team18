b98d1ef1ba69c348214aba33d807b9ef3ddb77bf
AgentE382 <AgentE382@gmail.com>
2016-11-07 10:56:41 -0500

    Fix URLs

--- SecureFileShare/messenger/urls.py
+++ SecureFileShare/messenger/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
-    url(r'^messenger/send_message', views.send, name='send'),
+    url(r'^messenger/send', views.send, name='send'),
+    url(r'^messenger/send_message', views.send_message, name='send_message'),
     url(r'^messenger/read', views.read, name='read')
 ]
