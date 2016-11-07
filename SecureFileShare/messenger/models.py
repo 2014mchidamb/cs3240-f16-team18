138604cf2eb5d3009647b203c18f8ff925938d6c
Gannon Combs <gc8um@virginia.edu>
2016-11-07 11:21:37 -0900

    Janky Encryption Method
    It's mainly a place holder.

--- SecureFileShare/messenger/models.py
+++ SecureFileShare/messenger/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from Crypto.PublicKey import RSA
+from Crypto import Random
 
 # Create your models here.
 class Message(models.Model):
@@ -8,3 +9,12 @@ class Message(models.Model):
     sender = models.OneToOneField(User, on_delete=models.Profile)
     recipient = models.OneToOneField(User, on_delete=models.Profile)
     encryptedFlag = models.BooleanField(default=False)
+
+def enc(message):
+    #note: probably shouldn't be creating a public key every time.
+    random_generator = Random.new().read
+    key = RSA.generate(1024, random_generator)
+    public_key = key.publickey()
+    enc_data = public_key.encrypt('abcdefgh', 32)
+    message.encryptedFlag = True
+
