dee50b88e66bc7285f1126a2b6eb76a4356fb4aa
Gannon Combs <gc8um@virginia.edu>
2016-11-06 18:39:25 -0900

    added message model

--- SecureFileShare/messenger/models.py
+++ SecureFileShare/messenger/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Message(models.Model):
+    text = models.TextField(max_length=500, blank=True)
+    sender = models.OneToOneField(User, on_delete=models.CASCADE)
+    recipient = models.OneToOneField(User, on_delete=models.CASCADE)
+    encryptedFlag = models.BooleanField(default=False)
