736d521d9991d427c9549069e6489b5b83f7461a
AgentE382 <AgentE382@gmail.com>
2016-11-07 10:52:05 -0500

    Add Messenger URLs

--- SecureFileShare/messenger/urls.py
+++ SecureFileShare/messenger/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
-    url(r'^messenger/send', views.send, name='send')
+    url(r'^messenger/send_message', views.send, name='send'),
+    url(r'^messenger/read', views.read, name='read')
 ]
